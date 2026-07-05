596ee35cc03b346cfe5e84b819548e082a5db797
EfratCoh <102877528+EfratCoh@users.noreply.github.com>
2026-07-05 19:49:37 +0300

    Delete consts/biomart.py

--- consts/biomart.py
+++
@@ -1,6 +0,0 @@
-
-
-from pathlib import Path
-
-BIOMART_DATA_PATH = Path("/sise/vaksler-group/IsanaRNA/miRNA_target_rules/benorgi/pipeline/data/biomart/")
-BIOMART_BLAST_PATH = Path("/sise/vaksler-group/IsanaRNA/miRNA_target_rules/benorgi/pipeline/data/biomart/blast/")
