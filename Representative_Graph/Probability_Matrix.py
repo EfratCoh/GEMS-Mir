b98fb27db2bf0852a7be52c87b18be991960e2e0
EfratCoh <102877528+EfratCoh@users.noreply.github.com>
2026-07-07 12:47:52 +0300

    Update Probability_Matrix.py

--- Representative_Graph/Probability_Matrix.py
+++ Representative_Graph/Probability_Matrix.py
@@ -67,7 +67,6 @@ class Probability_Matrix():
         try:
             return self.df_interactions.loc[0]['fragment']
         except:
-            print("errrrrrrrrrrrrrrrrrrrrrrrrrroorrrrrrrrrrrrrr")
             print(self.ID_interaction())
 
     def get_miRNA(self):
@@ -85,28 +84,7 @@ class Probability_Matrix():
 
     import numpy as np
 
-    # # ...
-    # Z = 0.0
-    # R = 1.987204258e-3  # kcal/mol/K  (אם תרצי להישאר עם 0.001987 זה גם בסדר)
-    # T = 310.15  # K
-    # beta = 1.0 / (R * T)
-    #
-    # # 1) מוצאים את Emin על פני כל תתי-האינטראקציות של הזוג הזה
-    # E_all = self.df_interactions['MFE'].to_numpy(dtype=float)
-    # E_all = E_all[np.isfinite(E_all)]
-    # Emin = E_all.min() if E_all.size else np.nan
-    #
-    # # 2) סכימה יציבה: e^{-beta*(E - Emin)} ואז מכפילים חזרה ב-e^{-beta*Emin}
-    # sum_exp = 0.0
-    # for index, row in self.df_interactions.iterrows():
-    #     free_energy = float(row['MFE'])  # kcal/mol
-    #     if not np.isfinite(free_energy):
-    #         continue
-    #     sum_exp += np.exp(-beta * (free_energy - Emin))
-    #
-    # Z = sum_exp * np.exp(-beta * Emin)
-    # return float(Z)
-
+  
     def probability_struct_dict(self):
         boltzmann_constant = 0.001987
         T = 310.15
@@ -151,29 +129,13 @@ class Probability_Matrix():
         for key, dp in self.dict_duplex().items():
             start_site_on_fragment = dp["start_site"]
             end_site_on_fragment = dp["end_site"]
-            # because we stay canon and non canon interaction there are not good interactions
-            # print(dp["duplex"]._mir_bulge)
-            # print(dp["duplex"]._mir_inter)
-            # print(dp["duplex"]._mrna_inter)
-            # print(dp["duplex"]._mrna_bulge)
-
+           
             basepair_list = [mir + dp["duplex"]._mrna_inter[i] for i, mir in dp["duplex"].mir_iterator() if dp["duplex"]._mrna_bulge[i]!="#"]
-            # print(basepair_list)
-            # print(len(basepair_list))
-            # print("############################################")
+           
             for i in range(len(basepair_list)):
                 if ' ' in basepair_list[i]:
                     continue
-                print("########################################")
-                print(end_site_on_fragment)
-                print(len(self.fragment))
-                print(i)
-                print("########################################")
-
                 self.metrix_position_basepair_interactions[i][end_site_on_fragment - i - 1].append(key)
-        # print("########################################################")
-        # self.display_matrix()
-        # print("")
 
     def matrix_insert_prob(self):
         for i in range(len(self.metrix_position_basepair_interactions) - 1, -1, -1):
@@ -206,8 +168,7 @@ def run_generate_matrices():
             # Check if the 'feature_step' folder exists
             if feature_step_path.exists() and feature_step_path.is_dir():
                 print(f"Entering folder: {feature_step_path}")
-                if "NPS_darnell_human_dataset" not in str(feature_step_path):
-                    continue
+                
                 fragment_site_list = []
 
                 for file in feature_step_path.iterdir():
@@ -243,48 +204,3 @@ def run_generate_matrices():
 
 
 # run_generate_matrices()
-
-def fragment_dist():
-    main_directory = ROOT_PATH_PHD_GOAL_ONE / "Data_Breath_Duplex/"
-
-    # Iterate over all directories and files within the main directory
-    for sub_dir in main_directory.iterdir():
-        count = 0
-        if sub_dir.is_dir():  # Check if it is a directory
-            feature_step_path = sub_dir / 'feature_step'
-
-            # Check if the 'feature_step' folder exists
-            if feature_step_path.exists() and feature_step_path.is_dir():
-                print(f"Entering folder: {feature_step_path}")
-
-                fragment_site_list = []
-
-                for file in feature_step_path.iterdir():
-                    print(f"Found file: {file.name}")
-                    last_dir = str(re.sub(r'_\d+', '', file.name).split(".csv")[0])
-
-                    if not "dataset" in file.name:
-                        last_dir = last_dir + "_dataset"
-
-                    file_path = os.path.join(feature_step_path, file)
-                    dataset_name = last_dir.split("_dataset")[0] + "_"
-                    MP = Probability_Matrix(Path(file_path), dataset_name)
-                    fragment_site_list.append(len(MP.get_fragment()))
-
-                # print("#################################################################################")
-                # print(fragment_site_list)
-                import matplotlib.pyplot as plt
-                import seaborn as sns
-
-                # Create a histogram with seaborn
-                sns.histplot(fragment_site_list, bins=10, kde=True)
-
-                # Set the title and labels
-                plt.title(file.name)
-
-                plt.xlabel('Size')
-                plt.ylabel('Frequency')
-
-                # Show the plot
-                plt.show()
-fragment_dist()
